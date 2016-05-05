f989d86d5ba55ed066a3d0c999a7afa132be427d
Sergio Chico <scc0031@alu.ubu.es>
2016-05-05 04:23:32 +0200

    Añadido wrapper para lista de URI y método para realizar persistencia.
    //TODO probar la creación correcta del fichero xml.

--- src/es/ubu/lsi/perikymata/MainApp.java
+++ src/es/ubu/lsi/perikymata/MainApp.java
@@ -2,31 +2,45 @@ package es.ubu.lsi.perikymata;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.URI;
+import java.util.List;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
 
 import es.ubu.lsi.perikymata.MainApp;
+import es.ubu.lsi.perikymata.modelo.URIListWrapper;
 import es.ubu.lsi.perikymata.vista.ImageFiltersController;
 import es.ubu.lsi.perikymata.vista.ImageSelectionController;
 import es.ubu.lsi.perikymata.vista.PerikymataCountController;
 import es.ubu.lsi.perikymata.vista.RootLayoutController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import javafx.stage.Stage;
 
 public class MainApp extends Application  {
 	 private Stage primaryStage;
 	 private BorderPane rootLayout;
 	 private Image imagenCompleta;
+	 private List<URI> imagenesURI;
 	   /**
 	     * Returns the main stage.
 	     * @return
 	     */
 	    public Stage getPrimaryStage() {
 	        return primaryStage;
+	        
 	    }
 
 	    public static void main(String[] args) {
@@ -48,6 +62,57 @@ public class MainApp extends Application  {
 
 		}
 		
+		 /**
+	     * Saves the current person data to the specified file.
+	     * 
+	     * @param file
+	     */
+	    public void saveURIsToFile(File file) {
+	        try {
+	            JAXBContext context = JAXBContext
+	                    .newInstance(URIListWrapper.class);
+	            Marshaller m = context.createMarshaller();
+	            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+	            // Wrapping our person data.
+	            URIListWrapper wrapper = new URIListWrapper();
+	            wrapper.setPersons(imagenesURI);
+
+	            // Marshalling and saving XML to the file.
+	            m.marshal(wrapper, file);
+	           
+	            // Save the file path to the registry.
+	            //TODO implementar setFilePath.
+	            //setPersonFilePath(file);
+	        } catch (Exception e) { // catches ANY exception
+	        	Alert alert = new Alert(Alert.AlertType.ERROR);
+	        	alert.setTitle("Error");
+	        	alert.setHeaderText("No se puede guardar archivo :\n" + file.getPath());
+	        	
+	        	Label label = new Label("La traza de la excepción fue:");
+	        	
+	        	StringWriter sw = new StringWriter();
+	        	PrintWriter pw = new PrintWriter(sw);
+	        	e.printStackTrace(pw);
+	        	
+	        	TextArea textArea = new TextArea(sw.toString());
+	        	textArea.setEditable(false);
+	        	textArea.setWrapText(true);
+
+	        	textArea.setMaxWidth(Double.MAX_VALUE);
+	        	textArea.setMaxHeight(Double.MAX_VALUE);
+	        	GridPane.setVgrow(textArea, Priority.ALWAYS);
+	        	GridPane.setHgrow(textArea, Priority.ALWAYS);
+
+	        	GridPane expContent = new GridPane();
+	        	expContent.setMaxWidth(Double.MAX_VALUE);
+	        	expContent.add(label, 0, 0);
+	        	expContent.add(textArea, 0, 1);
+	        	alert.getDialogPane().setExpandableContent(expContent);
+	        	alert.showAndWait();
+	        }
+	    }
+	    
 		/**
 	     * Initializes the root layout and tries to load the last opened
 	     * person file.
